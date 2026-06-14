52dc2f27cbabdb01cb614e962ae9ed1c47f36b72
Stephen Playford <slplayford@gmail.com>
2026-06-14 16:54:13 -0400

    write_file function added

---
+++ functions/write_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_path, file_path))
+
+        valid_target_file = os.path.commonpath([abs_path, target_file]) == abs_path
+
+        if not valid_target_file:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        if os.path.isdir(target_file):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+        os.makedirs(name=file_path, exist_ok=True)
+
+        with open(target_file, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except:
+        return f'Error: RuntimeError occured while running write_file()'
